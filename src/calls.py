d896819248b897e25cdeb5e08ed6661b5b5ed6be
Drew Gotshalk <32356221+The-Black-Star@users.noreply.github.com>
2021-03-05 01:41:22 -1000

    setting up files for calling github REST api and classest that will hold this information

---
+++ src/calls.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright © 2021 drewtg <drewtg@The-Black-Star>
+#
+# Distributed under terms of the MIT license.
+
+"""
+This file is where we will be doing the github calls.
+https://docs.github.com/en/rest/guides/getting-started-with-the-rest-api
+"""
+
+import requests
