6d4e78f458eceef181f6bafcc9cb0ca1f164c556
Drew Gotshalk <32356221+The-Black-Star@users.noreply.github.com>
2021-03-06 10:26:39 -1000

    set and defined basic functions for calls

--- src/calls.py
+++ src/calls.py
@@ -7,8 +7,18 @@
 # Distributed under terms of the MIT license.
 
 """
-This file is where we will be doing the github calls.
+This file is where we will be doing the github calls,
+This file will be used by the projects class to update all the inforamtion within the class
 https://docs.github.com/en/rest/guides/getting-started-with-the-rest-api
+https://docs.github.com/en/rest/reference
 """
 
 import requests
+
+class github_api:
+    def project_start(self, project):
+        pass
+    def project_branches(self, project):
+        pass
+    def project_commits(self, project):
+        pass
